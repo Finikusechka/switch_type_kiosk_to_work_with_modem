d5190475b1935a81a60cadcc7115a1c979f8fbd4
Vlad.K <vlad.k@mf-master.ru>
2022-08-24 14:47:51 +0300

    Add --valut-pasword-file to configure_kiosk_with_modem.sh

--- ansible/modem_kiosk/configure_kiosk_with_modem.sh
+++ ansible/modem_kiosk/configure_kiosk_with_modem.sh
@@ -9,13 +9,13 @@ read -a serial_kiosk -p 'Введите номер киоска > '
 full_name_ovpn_file=$ip'_'$town'_'$cinematheater
 echo $full_name_ovpn_file
 
-ansible-playbook -i "$dir_of_script"hosts.yml "$dir_of_script"create_and_dowload_ovpn.yaml  -e "fullnamekiosk=$full_name_ovpn_file" #Ансибл создает и забирает на локальный пк ovpn конфиг файл
+ansible-playbook -i "$dir_of_script"hosts.yml "$dir_of_script"create_and_dowload_ovpn.yaml  -e "fullnamekiosk=$full_name_ovpn_file" --vault-password-file /home/vlad/ansible/password_for_vault #Ансибл создает и забирает на локальный пк ovpn конфиг файл
 
 mv /home/vlad/seed240/home/denis/ovpns/"$full_name_ovpn_file".ovpn /home/vlad/vpn_file_for_kiosk/"$full_name_ovpn_file".conf #Переименовываем полученный файл для автозапуска ovpn
 
 echo -e "script-security 2 \nup /etc/openvpn/update-resolv-conf \ndown /etc/openvpn/update-resolv-conf" >> /home/vlad/vpn_file_for_kiosk/"$full_name_ovpn_file".conf # Добавляем необходимые настройки в конф файл
 
-ansible-playbook -i "$dir_of_script"inventory.ini -e "serial_kiosk=$serial_kiosk fullnamekiosk=$full_name_ovpn_file" "$dir_of_script"work_on_kiosk.yaml
+ansible-playbook -i "$dir_of_script"inventory.ini -e "serial_kiosk=$serial_kiosk fullnamekiosk=$full_name_ovpn_file" "$dir_of_script"work_on_kiosk.yaml --vault-password-file /home/vlad/ansible/password_for_vault
 
 
 
