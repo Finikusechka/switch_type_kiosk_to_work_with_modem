b432c16a58946b9c7641ea094dc3f420d878c352
vlad <vlad.k@mf-master.ru>
2022-11-21 13:00:36 +0300

    add var for have setting

--- ansible/modem_kiosk/configure_kiosk_with_modem.sh
+++ ansible/modem_kiosk/configure_kiosk_with_modem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+seed=seed244                                               # имя из hosts.yml
 dir_of_script=/home/vlad/git_projects/ansible/modem_kiosk/ # Дирректория де лежит ансибл проект
 ANSIBLE_CONFIG=/home/vlad/git_projects/ansible/modem_kiosk/ansible.cfg #Указываем ansible config file
 export ANSIBLE_CONFIG                                      # Экспортируем переменную в окружение
@@ -11,7 +12,7 @@ echo $full_name_ovpn_file
 
 ansible-playbook -i "$dir_of_script"hosts.yml "$dir_of_script"create_and_dowload_ovpn.yaml  -e "fullnamekiosk=$full_name_ovpn_file" --vault-password-file /home/vlad/ansible/password_for_vault #Ансибл создает и забирает на локальный пк ovpn конфиг файл
 
-mv /home/vlad/seed240/home/denis/ovpns/"$full_name_ovpn_file".ovpn /home/vlad/vpn_file_for_kiosk/"$full_name_ovpn_file".conf #Переименовываем полученный файл для автозапуска ovpn
+mv /home/vlad/"$seed"/home/denis/ovpns/"$full_name_ovpn_file".ovpn /home/vlad/vpn_file_for_kiosk/"$full_name_ovpn_file".conf #Переименовываем полученный файл для автозапуска ovpn
 
 echo -e "script-security 2 \nup /etc/openvpn/update-resolv-conf \ndown /etc/openvpn/update-resolv-conf" >> /home/vlad/vpn_file_for_kiosk/"$full_name_ovpn_file".conf # Добавляем необходимые настройки в конф файл
 
